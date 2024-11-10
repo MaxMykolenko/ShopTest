280318d444f3715b65e5f7304b7e5c4e4815443e
Max Mykolenko <72696352+MaxMykolenko@users.noreply.github.com>
2024-11-10 18:36:22 +0200

    Add files via upload

--- slider.js
+++ slider.js
@@ -5,6 +5,9 @@ document.addEventListener('DOMContentLoaded', function () {
     const swiper = new Swiper('.swiper', {
         loop: true,
         effect: 'fade',
+        fadeEffect: {
+            crossFade: true // Дозволяє плавну зміну без показу попереднього слайда
+        },
         navigation: {
             nextEl: '.swiper-button-next',
             prevEl: '.swiper-button-prev',
